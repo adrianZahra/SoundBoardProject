d850632c3d8228d117a479b26a861ecbe1e5a89f
adrianZahra <adrian.zahra@my.jcu.edu.au>
2016-11-21 09:35:06 +1000

    Initial commit

---
+++ src/BaseFrame.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by pccc on 11/21/2016.
+ */
+public class BaseFrame extends JFrame implements ActionListener {
+    
+
+
+    BaseFrame(){
+        super("Adrian's sound board");
+
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
